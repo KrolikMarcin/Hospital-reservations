feb7d5749998638aff1211ce5ee3b87f4a1f14d0
Marcin Krolik <krolikmarcin.mk@gmail.com>
2018-09-09 22:05:27 +0200

    Add navbar for patient and employee

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   root 'reservations#index'
   get 'bills/not_paid', to: 'bills#not_paid'
   devise_for :users
+  resources :bills, only: :index
   resources :reservations, shallow: true do
     get 'doctor_choice'
     patch 'doctor_choice_save'
-    resources :bills, only: [:show, :index]
+    resources :bills, only: :show
   end
-  resources :addresses, only: [:show, :new, :create, :index]
+  resources :addresses, only: [:show, :new, :edit, :create, :index]
   namespace :admin do
     resources :reservations, only: [:index, :show] do
       get 'change_status'
